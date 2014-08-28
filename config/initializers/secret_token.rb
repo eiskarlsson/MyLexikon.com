13581f82d5dffab6c1fc6b233c4588b0d83381c7
Ivan Pentchev <pentchev.ivan@gmail.com>
2014-08-28 13:40:01 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyLexikonCom::Application.config.secret_token = '62e333f3ead0d6735729b2410f93321ddd3c7decab91b4a0a3ed1b76ac8a6741e5d823d0ed20f81b7414161f78612401031f7c31fe0651217e0619c0ebde7bf5'
